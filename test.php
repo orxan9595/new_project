964096ade78e10fd6a0d369cc06bda23d981b7b8
orxan9595 <42065551+orxan9595@users.noreply.github.com>
2018-08-03 17:45:57 +0400

    Mecburi sekilde deywiklik eledim
    bezi meseler vardiki unudulkmusdu ve tedrisin elavesi ile yeni metn elave edildi

--- test.php
+++ test.php
@@ -43,6 +43,7 @@
             echo "oldu her sey";
             echo "Deyesen olmadi hesne ama baxmaq lazimdir.";
             echo "Sanki indi daha dogru oldu";
+            echo "Mecburi sekilde burda yene deywiklikler eledim.";
     }
 
 ?>
