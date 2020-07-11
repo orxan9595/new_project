76365409866700152de8d9ca96ef102ac18fada4
orxan9595 <42065551+orxan9595@users.noreply.github.com>
2020-07-11 15:54:07 +0400

    Genc bunu deyisdim men

--- test.php
+++ test.php
@@ -41,7 +41,7 @@
             echo $_POST["datetime"];
             echo $_POST["datetime"];
             echo "oldu her sey";
-            echo "yox bu olamli deyil"
+            echo "yox bu olamli deyildi"
     }
 
 ?>
